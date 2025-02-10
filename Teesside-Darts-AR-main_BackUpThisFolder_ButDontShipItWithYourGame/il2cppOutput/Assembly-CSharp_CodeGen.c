3a8eaa1bab19cfcbcb9db4484a7824265c216027
Meowmixforme <v8255920@live.tees.ac.uk>
2025-02-10 02:45:38 +0000

    Update 2
    Feather updated

---
+++ Teesside-Darts-AR-main_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,257 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Single ARFeatheredPlaneMeshVisualizer::get_featheringWidth()
+extern void ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D (void);
+// 0x00000002 System.Void ARFeatheredPlaneMeshVisualizer::set_featheringWidth(System.Single)
+extern void ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110 (void);
+// 0x00000003 System.Void ARFeatheredPlaneMeshVisualizer::Awake()
+extern void ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA (void);
+// 0x00000004 System.Void ARFeatheredPlaneMeshVisualizer::OnEnable()
+extern void ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0 (void);
+// 0x00000005 System.Void ARFeatheredPlaneMeshVisualizer::OnDisable()
+extern void ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8 (void);
+// 0x00000006 System.Void ARFeatheredPlaneMeshVisualizer::ARPlane_boundaryUpdated(UnityEngine.XR.ARFoundation.ARPlaneBoundaryChangedEventArgs)
+extern void ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F (void);
+// 0x00000007 System.Void ARFeatheredPlaneMeshVisualizer::GenerateBoundaryUVs(UnityEngine.Mesh)
+extern void ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5 (void);
+// 0x00000008 System.Void ARFeatheredPlaneMeshVisualizer::.ctor()
+extern void ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6 (void);
+// 0x00000009 System.Void ARFeatheredPlaneMeshVisualizer::.cctor()
+extern void ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3 (void);
+// 0x0000000A System.Void ARPlaneController::Awake()
+extern void ARPlaneController_Awake_m1342D858EA7B39DA666ADA00067AB858A4A898E9 (void);
+// 0x0000000B System.Void ARPlaneController::OnEnable()
+extern void ARPlaneController_OnEnable_mC7DAB1C912A21430CDDA590A541A12EC5A08CEAA (void);
+// 0x0000000C System.Void ARPlaneController::OnDisable()
+extern void ARPlaneController_OnDisable_mB3B1073F746D35E5464749620441725075FD0D14 (void);
+// 0x0000000D System.Void ARPlaneController::DisablePlaneDetection()
+extern void ARPlaneController_DisablePlaneDetection_m11F945B4A95A97BC2ED27CE610E606F1A6D17028 (void);
+// 0x0000000E System.Void ARPlaneController::SetAllPlanesActive(System.Boolean)
+extern void ARPlaneController_SetAllPlanesActive_m2170DD90B820E9F96B55D7AF5D53D3E045CAF024 (void);
+// 0x0000000F System.Void ARPlaneController::.ctor()
+extern void ARPlaneController__ctor_m0573BE0E4ACE42EB60FBC208F8A36EED966EF11E (void);
+// 0x00000010 System.Void DartControl::Start()
+extern void DartControl_Start_mB6FADA1C4D16632B2182BCE50F571C75A3D70809 (void);
+// 0x00000011 System.Void DartControl::OnEnable()
+extern void DartControl_OnEnable_m92089924191D478D69D4411C426F0E127270385C (void);
+// 0x00000012 System.Void DartControl::OnDisable()
+extern void DartControl_OnDisable_m961067868D3C0E4EDEC5BC2AE1BA5E0DC32CC460 (void);
+// 0x00000013 System.Void DartControl::Update()
+extern void DartControl_Update_m3C500D87AF6AF4845097265C14F73EA371EB5D6F (void);
+// 0x00000014 System.Void DartControl::DartsInit()
+extern void DartControl_DartsInit_m7274AE0BB9393779C178675CA8748A2B5F4DCB15 (void);
+// 0x00000015 System.Collections.IEnumerator DartControl::WaitAndSpawnDart()
+extern void DartControl_WaitAndSpawnDart_m874B9D90718028532A9C03F627CE4187F9B0FE4C (void);
+// 0x00000016 System.Void DartControl::.ctor()
+extern void DartControl__ctor_m7D44C12772F25FD192F7296C074479F40A1EE531 (void);
+// 0x00000017 System.Void DartControl/<WaitAndSpawnDart>d__20::.ctor(System.Int32)
+extern void U3CWaitAndSpawnDartU3Ed__20__ctor_m584BA1C66721E3392621CF400AAD96EB407E1761 (void);
+// 0x00000018 System.Void DartControl/<WaitAndSpawnDart>d__20::System.IDisposable.Dispose()
+extern void U3CWaitAndSpawnDartU3Ed__20_System_IDisposable_Dispose_mD2723EB605709D3A0817DA8BF69A2E0156B8A59B (void);
+// 0x00000019 System.Boolean DartControl/<WaitAndSpawnDart>d__20::MoveNext()
+extern void U3CWaitAndSpawnDartU3Ed__20_MoveNext_m7208839EE216869DC4AA600CE59F2C0E171DAC2D (void);
+// 0x0000001A System.Object DartControl/<WaitAndSpawnDart>d__20::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CWaitAndSpawnDartU3Ed__20_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m826966610C8666FDC30A71F4750F94735785A8B3 (void);
+// 0x0000001B System.Void DartControl/<WaitAndSpawnDart>d__20::System.Collections.IEnumerator.Reset()
+extern void U3CWaitAndSpawnDartU3Ed__20_System_Collections_IEnumerator_Reset_m233ECB6CA11095F365AD8A3838E34849E75974D1 (void);
+// 0x0000001C System.Object DartControl/<WaitAndSpawnDart>d__20::System.Collections.IEnumerator.get_Current()
+extern void U3CWaitAndSpawnDartU3Ed__20_System_Collections_IEnumerator_get_Current_mE9B10172941D386E32FEFC0112004D02CBA1A176 (void);
+// 0x0000001D System.Void PlaceDartboard::add_onPlacedObject(System.Action)
+extern void PlaceDartboard_add_onPlacedObject_m7FAB5D16530E148A554F91E3CE899B59868D3EBF (void);
+// 0x0000001E System.Void PlaceDartboard::remove_onPlacedObject(System.Action)
+extern void PlaceDartboard_remove_onPlacedObject_m75CED66122E3B46559EACB5E118B8B819D2FDCC0 (void);
+// 0x0000001F System.Void PlaceDartboard::Awake()
+extern void PlaceDartboard_Awake_mED2F9D5F22043468834B39C3214B1B5F29EB5C86 (void);
+// 0x00000020 System.Void PlaceDartboard::Update()
+extern void PlaceDartboard_Update_mD0FB670D661FA59445BEC7245F56B09947272072 (void);
+// 0x00000021 System.Void PlaceDartboard::UpdatePlacementPosition()
+extern void PlaceDartboard_UpdatePlacementPosition_m25E174082920D3E2F727AD4D448280062B86131E (void);
+// 0x00000022 System.Void PlaceDartboard::UpdatePlacementIndicator()
+extern void PlaceDartboard_UpdatePlacementIndicator_mCF5232ABF6E994A46C687BC2D4AC52CFA4691C69 (void);
+// 0x00000023 System.Void PlaceDartboard::PlaceObject()
+extern void PlaceDartboard_PlaceObject_mBE11354FC6EA0884F7ED6E18C9D75197598C6E49 (void);
+// 0x00000024 System.Void PlaceDartboard::.ctor()
+extern void PlaceDartboard__ctor_mE74330237002233EF07F00BBC08F88AA6ADD0844 (void);
+// 0x00000025 System.Void PlaceDartboard::.cctor()
+extern void PlaceDartboard__cctor_m62D17A5702A4E7DE168EC42BDF2DEE1EF10D8DBC (void);
+// 0x00000026 System.Void Quit::Start()
+extern void Quit_Start_m033A99F763D22B87236EB23E02243834B9CCF280 (void);
+// 0x00000027 System.Void Quit::Update()
+extern void Quit_Update_m0ACBFC7352DC28E4E3F0FD0CB563C8E717681F9B (void);
+// 0x00000028 System.Void Quit::QuitGame()
+extern void Quit_QuitGame_m576E696007EEE01FF50CCBF02F57BDB275102FF5 (void);
+// 0x00000029 System.Void Quit::.ctor()
+extern void Quit__ctor_m3F91DC1CD69CEDC58473E2984694380137C228C0 (void);
+// 0x0000002A System.Void Restart::RestartGame()
+extern void Restart_RestartGame_m8C2CF32B0F34BCAF4482EFAA3EF179CD3AD24F43 (void);
+// 0x0000002B System.Void Restart::.ctor()
+extern void Restart__ctor_m93B334B8C5F5E8202069056D44EDC3CC903B2CBB (void);
+// 0x0000002C System.Void ThrowDart::Start()
+extern void ThrowDart_Start_m6CD15A15C319759253C353785C001D2E1956FAA7 (void);
+// 0x0000002D System.Void ThrowDart::FixedUpdate()
+extern void ThrowDart_FixedUpdate_mC7527477805290CA728282EA35476F4365A3FABC (void);
+// 0x0000002E System.Collections.IEnumerator ThrowDart::InitDartDestroyVFX()
+extern void ThrowDart_InitDartDestroyVFX_mEAD1413486C60FC13D5972B9B01167EA45D9C30C (void);
+// 0x0000002F System.Void ThrowDart::OnTriggerEnter(UnityEngine.Collider)
+extern void ThrowDart_OnTriggerEnter_mC6F91449C124229657D25CD4642D01BBFD83D698 (void);
+// 0x00000030 System.Void ThrowDart::.ctor()
+extern void ThrowDart__ctor_m7D3A24DB5CCE58D6878C689F29026BF475B755C7 (void);
+// 0x00000031 System.Void ThrowDart/<InitDartDestroyVFX>d__11::.ctor(System.Int32)
+extern void U3CInitDartDestroyVFXU3Ed__11__ctor_m6D4D542933A38F54FD3A123A88F06D85F3DB2DC7 (void);
+// 0x00000032 System.Void ThrowDart/<InitDartDestroyVFX>d__11::System.IDisposable.Dispose()
+extern void U3CInitDartDestroyVFXU3Ed__11_System_IDisposable_Dispose_m2F303A7668A52B5F8C9316C7C4CDE4C967ECFF58 (void);
+// 0x00000033 System.Boolean ThrowDart/<InitDartDestroyVFX>d__11::MoveNext()
+extern void U3CInitDartDestroyVFXU3Ed__11_MoveNext_mC0DB6DEE5D31296DCD93AB48115ADEE0F6CA2AAA (void);
+// 0x00000034 System.Object ThrowDart/<InitDartDestroyVFX>d__11::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CInitDartDestroyVFXU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC23326FDA5B8375D941315FDED6D5992AD156496 (void);
+// 0x00000035 System.Void ThrowDart/<InitDartDestroyVFX>d__11::System.Collections.IEnumerator.Reset()
+extern void U3CInitDartDestroyVFXU3Ed__11_System_Collections_IEnumerator_Reset_mBCDBEB63076579F9D7AAB84EB14A001A929D3FEF (void);
+// 0x00000036 System.Object ThrowDart/<InitDartDestroyVFX>d__11::System.Collections.IEnumerator.get_Current()
+extern void U3CInitDartDestroyVFXU3Ed__11_System_Collections_IEnumerator_get_Current_m9C52959BAE4D19968FEE4664308F004C6B3188C5 (void);
+static Il2CppMethodPointer s_methodPointers[54] = 
+{
+	ARFeatheredPlaneMeshVisualizer_get_featheringWidth_m14D3A8BE3E9A745E6FD525B19ADDC892B8399B4D,
+	ARFeatheredPlaneMeshVisualizer_set_featheringWidth_mD616A09A3B426EA5DE1FA37334DD194E43EEC110,
+	ARFeatheredPlaneMeshVisualizer_Awake_mC5DB0414A2514BF4851266C25141C903F0AC57BA,
+	ARFeatheredPlaneMeshVisualizer_OnEnable_m8781C85CFED871C8A81A5B88DB1031856E0FC9F0,
+	ARFeatheredPlaneMeshVisualizer_OnDisable_m2343B05B1A8F14BAD4DD516C584281B66FE6A4E8,
+	ARFeatheredPlaneMeshVisualizer_ARPlane_boundaryUpdated_mB3D9BBD14EA1FE3ECDBACC2DB89C1B110B8B6B5F,
+	ARFeatheredPlaneMeshVisualizer_GenerateBoundaryUVs_mF756D3C1F7925A69CD8C7C8CCE56209AB321FEF5,
+	ARFeatheredPlaneMeshVisualizer__ctor_m9A77651BCAE58AA0B994FFF6C6B63B1CFF2729F6,
+	ARFeatheredPlaneMeshVisualizer__cctor_m56285A000EE27A1631BE9C5BCDA5C376E5D133C3,
+	ARPlaneController_Awake_m1342D858EA7B39DA666ADA00067AB858A4A898E9,
+	ARPlaneController_OnEnable_mC7DAB1C912A21430CDDA590A541A12EC5A08CEAA,
+	ARPlaneController_OnDisable_mB3B1073F746D35E5464749620441725075FD0D14,
+	ARPlaneController_DisablePlaneDetection_m11F945B4A95A97BC2ED27CE610E606F1A6D17028,
+	ARPlaneController_SetAllPlanesActive_m2170DD90B820E9F96B55D7AF5D53D3E045CAF024,
+	ARPlaneController__ctor_m0573BE0E4ACE42EB60FBC208F8A36EED966EF11E,
+	DartControl_Start_mB6FADA1C4D16632B2182BCE50F571C75A3D70809,
+	DartControl_OnEnable_m92089924191D478D69D4411C426F0E127270385C,
+	DartControl_OnDisable_m961067868D3C0E4EDEC5BC2AE1BA5E0DC32CC460,
+	DartControl_Update_m3C500D87AF6AF4845097265C14F73EA371EB5D6F,
+	DartControl_DartsInit_m7274AE0BB9393779C178675CA8748A2B5F4DCB15,
+	DartControl_WaitAndSpawnDart_m874B9D90718028532A9C03F627CE4187F9B0FE4C,
+	DartControl__ctor_m7D44C12772F25FD192F7296C074479F40A1EE531,
+	U3CWaitAndSpawnDartU3Ed__20__ctor_m584BA1C66721E3392621CF400AAD96EB407E1761,
+	U3CWaitAndSpawnDartU3Ed__20_System_IDisposable_Dispose_mD2723EB605709D3A0817DA8BF69A2E0156B8A59B,
+	U3CWaitAndSpawnDartU3Ed__20_MoveNext_m7208839EE216869DC4AA600CE59F2C0E171DAC2D,
+	U3CWaitAndSpawnDartU3Ed__20_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m826966610C8666FDC30A71F4750F94735785A8B3,
+	U3CWaitAndSpawnDartU3Ed__20_System_Collections_IEnumerator_Reset_m233ECB6CA11095F365AD8A3838E34849E75974D1,
+	U3CWaitAndSpawnDartU3Ed__20_System_Collections_IEnumerator_get_Current_mE9B10172941D386E32FEFC0112004D02CBA1A176,
+	PlaceDartboard_add_onPlacedObject_m7FAB5D16530E148A554F91E3CE899B59868D3EBF,
+	PlaceDartboard_remove_onPlacedObject_m75CED66122E3B46559EACB5E118B8B819D2FDCC0,
+	PlaceDartboard_Awake_mED2F9D5F22043468834B39C3214B1B5F29EB5C86,
+	PlaceDartboard_Update_mD0FB670D661FA59445BEC7245F56B09947272072,
+	PlaceDartboard_UpdatePlacementPosition_m25E174082920D3E2F727AD4D448280062B86131E,
+	PlaceDartboard_UpdatePlacementIndicator_mCF5232ABF6E994A46C687BC2D4AC52CFA4691C69,
+	PlaceDartboard_PlaceObject_mBE11354FC6EA0884F7ED6E18C9D75197598C6E49,
+	PlaceDartboard__ctor_mE74330237002233EF07F00BBC08F88AA6ADD0844,
+	PlaceDartboard__cctor_m62D17A5702A4E7DE168EC42BDF2DEE1EF10D8DBC,
+	Quit_Start_m033A99F763D22B87236EB23E02243834B9CCF280,
+	Quit_Update_m0ACBFC7352DC28E4E3F0FD0CB563C8E717681F9B,
+	Quit_QuitGame_m576E696007EEE01FF50CCBF02F57BDB275102FF5,
+	Quit__ctor_m3F91DC1CD69CEDC58473E2984694380137C228C0,
+	Restart_RestartGame_m8C2CF32B0F34BCAF4482EFAA3EF179CD3AD24F43,
+	Restart__ctor_m93B334B8C5F5E8202069056D44EDC3CC903B2CBB,
+	ThrowDart_Start_m6CD15A15C319759253C353785C001D2E1956FAA7,
+	ThrowDart_FixedUpdate_mC7527477805290CA728282EA35476F4365A3FABC,
+	ThrowDart_InitDartDestroyVFX_mEAD1413486C60FC13D5972B9B01167EA45D9C30C,
+	ThrowDart_OnTriggerEnter_mC6F91449C124229657D25CD4642D01BBFD83D698,
+	ThrowDart__ctor_m7D3A24DB5CCE58D6878C689F29026BF475B755C7,
+	U3CInitDartDestroyVFXU3Ed__11__ctor_m6D4D542933A38F54FD3A123A88F06D85F3DB2DC7,
+	U3CInitDartDestroyVFXU3Ed__11_System_IDisposable_Dispose_m2F303A7668A52B5F8C9316C7C4CDE4C967ECFF58,
+	U3CInitDartDestroyVFXU3Ed__11_MoveNext_mC0DB6DEE5D31296DCD93AB48115ADEE0F6CA2AAA,
+	U3CInitDartDestroyVFXU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mC23326FDA5B8375D941315FDED6D5992AD156496,
+	U3CInitDartDestroyVFXU3Ed__11_System_Collections_IEnumerator_Reset_mBCDBEB63076579F9D7AAB84EB14A001A929D3FEF,
+	U3CInitDartDestroyVFXU3Ed__11_System_Collections_IEnumerator_get_Current_m9C52959BAE4D19968FEE4664308F004C6B3188C5,
+};
+static const int32_t s_InvokerIndices[54] = 
+{
+	4361,
+	3608,
+	4419,
+	4419,
+	4419,
+	3480,
+	3576,
+	4419,
+	6965,
+	4419,
+	4419,
+	4419,
+	4419,
+	3504,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4323,
+	4419,
+	3553,
+	4419,
+	4255,
+	4323,
+	4419,
+	4323,
+	6846,
+	6846,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	6965,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4419,
+	4323,
+	3576,
+	4419,
+	3553,
+	4419,
+	4255,
+	4323,
+	4419,
+	4323,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	54,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
